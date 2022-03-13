3daecb7bd8e41d9f66eafda7ad13200a833c61e4
Alex Pc <Alex Pc@Alex-HP>
2022-03-13 21:35:13 +0100

    Main

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,11 +1,12 @@
 package org.iesalandalus.programacion.reservasaulas;
 import org.iesalandalus.programacion.reservasaulas.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.reservasaulas.mvc.controlador.IControlador;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.Modelo;
 import org.iesalandalus.programacion.reservasaulas.mvc.vista.IVista;
 import org.iesalandalus.programacion.reservasaulas.mvc.vista.Vista;
 
-public class MainApp implements IVista,IModelo,IControlador {
+public class MainApp {
 
 	public static void main(String[] args) {
 		IModelo modelo = new Modelo();
